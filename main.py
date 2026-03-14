17a33ffd899aadf9b5275204922e490398716998
Victoria Coleman <toryecoleman@gmail.com>
2026-03-14 13:13:50 +0000

    Keep player's turn going if they get what they asked for.

--- main.py
+++ main.py
@@ -18,10 +18,9 @@ def main():
     #Print game instructions and ask user if they want to play.
     playing = printInstructions(gameInstructions)
 
-    #User will always be the first player
-    userTurn = True
-
-    while playing:
+    while playing: #Each loop is a GAME of GO Fish
+        # User will always be the first player
+        userTurn = True
 
         #This creates the pond of cards that can be drawn from. This is the deck that can be altered as the game is played.
         pond = list(DECK)
@@ -36,9 +35,19 @@ def main():
 
         #User and computer will continue taking turns until endgame conditions are met.
         gameOver = False
+        #This flag will be used to determine if a loop is a continuation of player turn
+        #(because they got what they asked for)
+        keepFishing = False
+
         while not gameOver:
             #Print out whose turn it is and current scores.
-            startOfTurn(userTurn, userScore, compScore)
+            if not keepFishing:
+                startOfTurn(userTurn, userScore, compScore)  # only print start of turn message if we switched players
+            else:
+                if userTurn:
+                    print("\nYou got what you asked for. You get to go again!")
+                else:
+                    print("\nYour opponent got what they asked for. They get to go again!")
 
             #Check whose turn it is and determine which rank to fish for
             if userTurn:
@@ -57,10 +66,15 @@ def main():
             #If any cards are taken, add to player's hand. Else, go fish
             if cardsTaken:
                 playerHand += cardsTaken
+                keepFishing = True
             else:
                 card = goFish(pond, userTurn)
                 pond.remove(card)
                 playerHand.append(card)
+                if card["rank"] == rank:
+                    keepFishing = True
+                else:
+                    keepFishing = False
 
             #Search for sets of 4 matching cards and update the score
             pointsGained = checkForBooks(userTurn, playerHand)
@@ -72,15 +86,16 @@ def main():
             #Check whether any endgame conditions have been met
             gameOver = checkForEndgame(userScore, compScore, userHand, compHand, pond)
 
-            #Switch active player - will flip the boolean value of the variable
-            userTurn = not userTurn
+            #Switch active player if fishing unsuccessful
+            if not keepFishing:
+                userTurn = not userTurn
 
         #If endgame conditions have been met, print score and who won
         displayGameResults(userScore, compScore)
 
         #Check if the user wants to play another round.
         if continuePlaying():
-            print("Great! Let's start another round of Go Fish!")
+            print("Great! Here we go!")
         else:
             playing = False
 
